e42208a77fba57aab908898b5d32be725977f713
Loi Tran <88891057+loi9@users.noreply.github.com>
2026-08-30 10:49:32 +0700

    Update config.js
    V13 sale

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Dán URL Web App của Google Apps Script vào đây sau khi triển khai Code.gs.
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxsoJg1sDMt6MiHJ6A4QleKr7KHZpf3X_tsafEi0s3ViVOjcBJUs83VK18H1hcM7FeGlQ/exec";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyQXqYIOJlfIukad36E_bi3R4dlCInw5N6t126qIdAQefD9xo00D-wuWcnhnRsiMqigbw/exec";
